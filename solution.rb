09e988b58516d7825cdbe0e3ceddfb7474b28e60
Andrew Patterson <andrew.patterson.3001@gmail.com>
2016-02-08 10:22:39 -0800

    add pseudocode

---
+++ solution.rb
@@ -0,0 +1,8 @@
+#NRI Back End Challenge Solution
+
+#################################
+#Pseudocode
+#1. Input from the user is an integer greater than 0 and their student id
+#2. Output from the user is a list of question id's that samples from strands and standards equally while adjusting question difficulty to the student's previous responses.
+##################################
+
