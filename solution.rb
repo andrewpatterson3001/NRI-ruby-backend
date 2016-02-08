535d70dd392bb09cf7ff602337bc5ca3752aa9c1
Andrew Patterson <andrew.patterson.3001@gmail.com>
2016-02-08 11:37:58 -0800

    allow for user input of number of questions and quiz creation with that many questions

--- solution.rb
+++ solution.rb
@@ -6,11 +6,26 @@
 #2. Output from the user is a list of question id's that samples from strands and standards equally while adjusting question difficulty to the student's previous responses.
 ##################################
 
+require 'csv'
+
+example_questions = CSV.read('questions.csv')
+
+example_questions.each do |question_data|
+  #create key value pairs to make selecting appropriate questions even easier
+end
+
 class Student
   def initialize(options = {})
     @student_id = options[:student_id]
     @standard_mastery = options[:standard_mastery]
-    @quizzes = options[:quizzes]
+    @quizzes = []
+    add_quiz()
+  end
+
+  def add_quiz
+    p "Please enter the number of questions you would like to answer as a whole number:"
+    question_number_input = gets
+    Quiz.new(number_of_questions: question_number_input)
   end
 end
 
@@ -18,11 +33,16 @@ class Quiz
   def initialize(options = {})
     @number_of_questions = options[:number_of_questions]
     @questions = []
+    add_questions(@number_of_questions)
   end
 
-  def add_question(new_question)
-    @questions << new_question
+  def add_questions(number_of_questions)
+    number_of_questions.times do
+      new_question = Question.new(question_id:, difficulty:, standard_id:, strand_id:)
+      @questions << new_question
   end
+
+
 end
 
 class Question
@@ -31,16 +51,14 @@ class Question
     @difficulty = options[:difficulty]
     @standard_id = options[:standard_id]
     @strand_id = options[:strand_id]
-    @assigned_hours_ago = options[:assigned_hours_ago]
-    @answered_hours_ago = options[:answered_hours_ago]
   end
 end
 
 #################################
 #Test Code
 #######assume Roy has student_id "1" and he enters "4" as the number of questions he wants to answer. He has never answered a quiz before so his standard mastery is null
-roy = Student.new(student_id:1)
-english_quiz = Quiz.new(number_of_questions: 4)
+roy_quiz = Student.new(student_id:1)
+#Roy enters "4" on the command line
 ####### The output should be something like [1,3,7,9]
 
 
